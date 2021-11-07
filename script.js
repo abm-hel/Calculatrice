22a4a5c2f330dd520c4658951136a0dbc72a395f
Adil BEN MOUSSA <73439684+abmbe@users.noreply.github.com>
2021-11-07 04:12:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-/*function insert(num) {
-	$('.result').val($('.result').val() + num);
-}
-
-function effacerEcran(){
-	$('.result').val('');
-}
-
-function effacer() {
-	value = $('.result').val();
-	$('.result').val(value.substring(0, value.length - 1));
-}
-
-function resultat() {
-	$('.result').val(eval($('.result').val()));
-}*/
-
-
 $(document).ready(function() 
 {
 	var result = 0;
-	var prevEntry = 0;
-	var operation = null;
 	var currentEntry = '0';
 	updateScreen(result);
-	var i = 0;
+
+	$(document).keydown(function(e){
+		e.preventDefault();
+	  });
 	
 	
 	$('button').on('click', function(evt) 
@@ -36,19 +19,19 @@ $(document).ready(function()
 		result = 0;
 		currentEntry = '0';
 	  } 
-	  /*else if (buttonPressed === "CE") 
-	  {
-		currentEntry = '0';
-	  }*/
-	  
+
 	  else if (buttonPressed === "DEL") 
 	  {
-		currentEntry = currentEntry.substring(0, currentEntry.length-1);
+		  if(currentEntry !="0")
+			currentEntry = currentEntry.substring(0, currentEntry.length-1);
 	  } 
 	  
 	  else if (buttonPressed === "+/-") 
 	  {
-		currentEntry *= -1;
+		if(currentEntry!=0)
+		{
+			currentEntry *= -1;
+		}
 	  } 
 	  
 	  else if (buttonPressed === '.')
@@ -56,37 +39,24 @@ $(document).ready(function()
 		currentEntry += '.';
 	  } 
 	  
-	  else if (isNumber(buttonPressed))
-	  {
-		if (currentEntry === '0') 
-			currentEntry = buttonPressed;
-		else 
-			currentEntry = currentEntry + buttonPressed;
-	  } 
-	  
-	  else if (isOperator(buttonPressed)) 
-	  {
-		prevEntry = parseFloat(currentEntry);
-		operation = buttonPressed;
-		currentEntry = '';
-		i=1;
-	  } 
-	  
 	  else if(buttonPressed === '%') 
 	  {
 		currentEntry = currentEntry / 100;
 	  } 
 
-	  else if (buttonPressed === '=')
-	  {
-		  if(i==1)
-		  {
-			currentEntry = operate(prevEntry, currentEntry, operation);
-			operation = null;
-			i=0;
-		  }
-	  }
-	  
+    else if(currentEntry === '0') 
+    {
+      currentEntry = buttonPressed;
+    }
+    else if(buttonPressed === '=') 
+    {
+      currentEntry = eval($('.screen').val());
+    }
+		else 
+    {
+      currentEntry = currentEntry + buttonPressed;
+    }
+
 	  updateScreen(currentEntry);
 	}
 
@@ -95,33 +65,7 @@ $(document).ready(function()
   
   updateScreen = function(displayValue) 
   {
-	var displayValue = displayValue.toString();
-	$('.screen').val(displayValue);
+    displayValue = displayValue.toString();
+    $('.screen').val(displayValue);
   };
   
-  isNumber = function(value)
-  {
-	return !isNaN(value);
-  }
-  
-  isOperator = function(value) 
-  {
-	return value === '/' || value === '*' || value === '+' || value === '-';
-  };
-  
-  operate = function(a, b, operation) 
-  {
-	a = parseFloat(a);
-	b = parseFloat(b);
-	
-	console.log(a, b, operation);
-
-	if (operation === '+')
-	 	return a + b;
-	else if (operation === '-') 
-		return a - b;
-	else if (operation === '*')
-		return a * b;
-	else if (operation === '/') 
-		return a / b;	
-  }
